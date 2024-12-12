a86a5e77e6e9390f538968df0fc36460ec870b3f
hache231 <idrisnsimba46@gmail.com>
2024-12-12 14:58:55 +0100

    Add station model with fields for name, owner, location, and hours; create initial migration

--- geoportal/models.py
+++ geoportal/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class station(models.Model):
+    name = models.CharField(max_length=100)
+    owner = models.CharField(max_length=100)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    Storage_capacity = models.FloatField()
+    Opening_hours = models.TimeField()
+    closing_hours = models.TimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+
+    
+
