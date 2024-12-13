fb070dd2d3d2d9d4c660fdc9d231450d118b5f86
hache231 <idrisnsimba46@gmail.com>
2024-12-13 18:26:13 +0100

    Register station model in admin and enhance map view to include all stations

--- geoportal/views.py
+++ geoportal/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from geoportal.models import station
+
 
 # Create your views here.
 def about(request):
     return render(request, 'about.html')
 
 def map(request):
-    return render(request, 'map.html')
+    stations = station.objects.all()
+    return render(request, 'map.html', {'stations': stations})
