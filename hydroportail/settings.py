a70b092769480f31ef9cd3728768a0c11f92102d
hache231 <idrisnsimba46@gmail.com>
2024-12-16 15:03:43 +0100

    change debug mode

--- hydroportail/settings.py
+++ hydroportail/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--vrld_)#a0_2r0yx6cy04!f68$!-_c1$h8s$n(gva4-$qmj&li'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['3.19.37.176','127.0.0.1']
 
@@ -123,7 +123,6 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'accounts', 'static'),
     os.path.join(BASE_DIR, 'geoportal', 'static'),
-    os.path.join(BASE_DIR, 'import_export', 'static'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
